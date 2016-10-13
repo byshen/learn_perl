ee8e97b9416d6be90b620db6c84c9c950c5b4809
root <root@localhost.localdomain>
2016-10-13 17:23:04 +0800

    fix bugs in variables.pl

--- variables.pl
+++ variables.pl
@@ -3,16 +3,13 @@
 
 my $str1 = "test string";
 
-my $string = "my string";
+my $str2 = "my string";
 
-print $str1 . $string
+print $str1 . $str2;
 
-# cannot judge a string is a number or string
-# for  number:
-# <,>,>=,<=,!=,+,<==>,*
-# for string:
-# lt,gt,fe,le,eq,cmp, . , X
-#------------------------------------------------#
+print "\n";
+
+
 my @test_arr = (
 	"111",
 	"222",
@@ -23,14 +20,25 @@ print $test_arr[0];
 print $test_arr[3];# warning
 
 print $test_arr[-1]; # this is fun like python
+
+print "\n";
+
 #------------------------------------------------#
 
+# cannot judge a string is a number or string
+# for  number:
+# <,>,>=,<=,!=,+,<==>,*
+# for string:
+# lt,gt,fe,le,eq,cmp, . , X
+#------------------------------------------------
+
 my %hash_tab = (
-	"key1" ==> "val1",
-	"key2" ==> "val2",
+	"key1" => "val1",
+	"key2" => "val2",
 );
 
 print $hash_tab{"key1"};
+print "\n";
 
 #------------------------------------------------#
 #lists
@@ -42,12 +50,12 @@ print $hash_tab{"key1"};
 	"in",
 	"the",
 	"work"
-)
+);
 
 (
 	"key1" =>  "val1",
 	"key2" =>  "val2",
-)
+);
 
 # list only exists temporarily, which can be given to a array or hash
 
